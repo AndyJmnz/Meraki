1a464a9da6f1e51f03515dc9d9f8692ea388e04d
IvanRA46 <ivannoeramirezvivanco@gmail.com>
2025-10-17 20:00:13 -0600

    Refactor code structure for improved readability and maintainability

--- index.php
+++ index.php
@@ -32,6 +32,45 @@
                 <img src="img/ImagenPortada1.png" alt="Edificio moderno">
             </div>
         </div>
+
+        <div class="about-word">ABOUT</div>
+        <!-- Nueva sección dividida -->
+        <section class="about-section">
+            <div class="about-left">
+                <h2 class="subtitle2">GRUPO MERAKI</h2>
+                <p>Somos Grupo Meraki BTO, consultoría especializada en la Gestión 360 de Proyectos Inmobiliarios. Con sede en Guadalajara, operamos con la filosofia</p>
+                <p><b>"Made with passion".</b></p>
+                <p>Nuestro compromiso es ofrecer asesoría integral que abarca desde la conceptualización inicial hasta la ejecución exitosa de tu proyecto. No solo coordinamos procesos; infundimos alma y creatividad en cada desarrollo.</p>
+            </div>
+            
+            <div class="about-divider"></div>
+              <div class="about-right">
+                <h2>Nos especializamos</h2>                
+                <div class="features-container">
+                    <div class="features-list">
+                        <div class="feature-item">
+                            <img src="img/inversion.png" alt="Icono" class="feature-icon">
+                            <span>Viabilidad Financiera y de Negocio</span>
+                        </div>
+                        <div class="feature-item">
+                            <img src="img/gancho.png" alt="Icono" class="feature-icon">
+                            <span>Viabilidad Financiera y de Negocio</span>
+                        </div>
+                        <div class="feature-item">
+                            <img src="img/balanza.png" alt="Icono" class="feature-icon">
+                            <span>Viabilidad Financiera y de Negocio</span>
+                        </div>
+                        <div class="feature-item">
+                            <img src="img/denver.png" alt="Icono" class="feature-icon">
+                            <span>Viabilidad Financiera y de Negocio</span>
+                        </div>
+                    </div>
+                    <div class="features-image">
+                        <img src="img/merakicss.png" alt="Nuestra empresa" class="right-image">
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </body>
 </html>
